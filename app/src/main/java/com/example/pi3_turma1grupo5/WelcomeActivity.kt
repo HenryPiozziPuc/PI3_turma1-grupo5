02a0894b5bd03d7e25c29e7ee86a2469caa3ab04
Henry Piozzi <henry.gp@puccampinas.edu.br>
2025-04-27 12:31:26 -0300

    Background e semicirculo

--- app/src/main/java/com/example/pi3_turma1grupo5/WelcomeActivity.kt
+++ app/src/main/java/com/example/pi3_turma1grupo5/WelcomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,6 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pi3_turma1grupo5.ui.theme.PI3_turma1grupo5Theme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class WelcomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +29,57 @@ class WelcomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PI3_turma1grupo5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting4(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                WelcomeScreenPreview()
             }
         }
     }
 }
 
 @Composable
-fun Greeting4(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WelcomeScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(
+            brush = Brush.verticalGradient(
+                colorStops = arrayOf(
+                    0.0f to Color(0xFFD2D5E2),
+                    0.33f to Color(0xFFD2D5E2),
+                    0.69f to Color(0xFF5770AF),
+                    0.90f to Color(0xFF455E9E),
+                    1.0f to Color(0xFF1033BC)
+                )
+            )
+        )
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp)
+                .align(Alignment.BottomCenter)
+            ) {
+        val canvasWidth = size.width
+        val canvasHeight = size.height
+
+        drawArc(
+            color = Color(0xFFCEE1F6).copy(alpha = 0.75f),
+            startAngle = 180f,
+            sweepAngle = 180f,
+            useCenter = true,
+            topLeft = androidx.compose.ui.geometry.Offset(
+                x = -(canvasWidth / 2),
+                y = 0f
+            ),
+            size = androidx.compose.ui.geometry.Size(
+                width = canvasWidth * 2,
+                height = canvasHeight * 2
+            )
+        )
+    }
+}
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview4() {
-    PI3_turma1grupo5Theme {
-        Greeting4("Android")
-    }
+fun WelcomeScreenPreview() {
+    WelcomeScreen()
 }
