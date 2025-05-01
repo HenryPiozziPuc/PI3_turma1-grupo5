e0eb135f0c96d5425636bf86e5443091a810c4d3
Brunoreit <bruno.rf2@puccampinas.edu.br>
2025-04-30 21:23:21 -0300

    class LoginActivity

--- app/src/main/java/com/example/pi3_turma1grupo5/LoginActivity.kt
+++ app/src/main/java/com/example/pi3_turma1grupo5/LoginActivity.kt
@@ -1,5 +1,9 @@
 package com.example.pi3_turma1grupo5
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -18,6 +22,19 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview // <-- IMPORTANTE pra funcionar o Preview!
+import com.example.pi3_turma1grupo5.ui.theme.PI3_turma1grupo5Theme
+
+class LoginActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PI3_turma1grupo5Theme {
+                LoginScreenPreview()
+            }
+        }
+    }
+}
 
 @Composable
 fun LoginScreen() {
