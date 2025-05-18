7ed34ebd8c2cb6d2c612406491a53a9e2a5b0c58
Henry Piozzi <henry.gp@puccampinas.edu.br>
2025-05-18 19:01:45 -0300

    mudança nas cores

--- app/src/main/java/com/example/pi3_turma1grupo5/ui/screens/SignUpActivity.kt
+++ app/src/main/java/com/example/pi3_turma1grupo5/ui/screens/SignUpActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -79,11 +78,7 @@ fun SignUpScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(
-                brush = Brush.verticalGradient(
-                    colorStops = BackgroundLight
-                )
-            )
+            .background(BackgroundLight)
     ) {
         Column(
             modifier = Modifier
