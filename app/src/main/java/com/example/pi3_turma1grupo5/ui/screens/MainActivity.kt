c9724e8bf972fd724f9b7d9319fd01e06acddeb4
Brunoreit <bruno.rf2@puccampinas.edu.br>
2025-05-31 22:15:43 -0300

    Refresh da tela main ao arrastar o  dedo para baixo

--- app/src/main/java/com/example/pi3_turma1grupo5/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/pi3_turma1grupo5/ui/screens/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
@@ -29,11 +30,16 @@ import com.example.pi3_turma1grupo5.model.ClasseSenha
 import com.example.pi3_turma1grupo5.ui.components.AdicionarCategoriaScreen
 import com.example.pi3_turma1grupo5.ui.components.AdicionarSenhaScreen
 import com.example.pi3_turma1grupo5.ui.components.MoldeCategoria
-import com.example.pi3_turma1grupo5.ui.components.MoldeSenha
 import com.example.pi3_turma1grupo5.ui.theme.PI3_turma1grupo5Theme
 import com.example.pi3_turma1grupo5.ui.theme.SoftGray
 import com.example.pi3_turma1grupo5.utils.BuscarCategorias
 import com.example.pi3_turma1grupo5.utils.BuscarSenhas
+import com.example.pi3_turma1grupo5.utils.refreshAll
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshState
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+
+
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
@@ -66,6 +72,9 @@ fun MainScreen() {
 
     val listaSenhas = remember { mutableStateListOf<ClasseSenha>() } // lista definitiva
     val listaCategorias = remember {mutableStateListOf<String>()}
+
+    var isRefreshing by remember {mutableStateOf(false)}
+
 
     if(Firebase.auth.currentUser == null){
         Text("Usuário não logado")
@@ -172,51 +181,69 @@ fun MainScreen() {
             }
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .fillMaxSize()
-                .background(SoftGray)
-                .verticalScroll(rememberScrollState()) // permite a rolagem da tela
-                .padding(horizontal = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
-        ){
-            Text(
-                modifier = Modifier.padding(top = 8.dp, bottom = 4.dp),
-                text = "Suas senhas:",
-                style = MaterialTheme.typography.headlineSmall, //small para as seções
-                fontWeight = FontWeight.SemiBold
+        // SwipeRefresh: reage ao usuario arrastar a tela para baixo (ativação)
+        SwipeRefresh( // ao puxar a tela para o refresh, é aqui que a operação se inicia
+            state = SwipeRefreshState(isRefreshing = isRefreshing),
+            onRefresh = { refreshAll(
+                onStart = { isRefreshing = true }, // mostra o icone de loading
+                onComplete = { isRefreshing = false }, // esconde o icone
+                onCategoriesLoaded = { categories ->
+                    listaCategorias.clear()
+                    listaCategorias.addAll(categories) // aqui é recomposta a tela
+                },
+                onPasswordsLoaded = { passwords ->
+                    listaSenhas.clear()
+                    listaSenhas.addAll(passwords)
+                }
             )
+        }
+        ){
+             Column(
+                 modifier = Modifier
+                     .padding(innerPadding)
+                     .fillMaxSize()
+                     .background(SoftGray)
+                     .verticalScroll(rememberScrollState()) // permite a rolagem da tela
+                     .padding(horizontal = 16.dp),
+                 verticalArrangement = Arrangement.spacedBy(16.dp)
+             ){
+                 Text(
+                     modifier = Modifier.padding(top = 8.dp, bottom = 4.dp),
+                     text = "Suas senhas:",
+                     style = MaterialTheme.typography.headlineSmall, //small para as seções
+                     fontWeight = FontWeight.SemiBold
+                 )
 
-            listaCategorias.forEach { categoria ->
-                MoldeCategoria(
-                    titulo = categoria,
-                    listaSenhas = listaSenhas
-                )
+                 listaCategorias.forEach { categoria ->
+                     MoldeCategoria(
+                         titulo = categoria,
+                         listaSenhas = listaSenhas
+                     )
+                 }
+             }
+
+             // abre o compose da nova senha e cria o callback "onSenhaAdicionada"
+             if(mostrarAddSenha) {
+                 AdicionarSenhaScreen(
+                     onBack = {mostrarAddSenha = false},
+                     onSenhaAdicionada = { novaSenha ->
+                         listaSenhas.add(novaSenha)
+
+                     }
+                 )
+             }
+
+            if(mostrarAddCategoria){
+                 AdicionarCategoriaScreen(
+                     onBack = {mostrarAddCategoria = false},
+                     onCategoriaAdicionada = {novaCategoria ->
+                         listaCategorias.add(novaCategoria)
+                         }
+                     )
+                 }
             }
         }
-
-        // abre o compose da nova senha e cria o callback "onSenhaAdicionada"
-        if(mostrarAddSenha) {
-            AdicionarSenhaScreen(
-                onBack = {mostrarAddSenha = false},
-                onSenhaAdicionada = { novaSenha ->
-                    listaSenhas.add(novaSenha)
-
-                }
-            )
-        }
-
-       if(mostrarAddCategoria){
-            AdicionarCategoriaScreen(
-                onBack = {mostrarAddCategoria = false},
-                onCategoriaAdicionada = {novaCategoria ->
-                    listaCategorias.add(novaCategoria)
-                }
-            )
-        }
     }
-}
 
 
 @Preview(showBackground = true)
